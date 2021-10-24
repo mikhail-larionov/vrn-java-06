44f0bd9d18686ff032315f4c2633e14d56e82442
Mihail <mynote02012014@gmail.com>
2021-10-24 17:10:46 +0300

    main refactoring

--- src/main/java/ru/dataart/academy/java/Main.java
+++ src/main/java/ru/dataart/academy/java/Main.java
@@ -2,13 +2,11 @@ package ru.dataart.academy.java;
 
 public class Main {
     public static void main(String[] args) {
-//        ClassLoader classLoader = Main.class.getClassLoader();
-//        System.out.println(classLoader.getResource("C:\\Users\\user\\Desktop\\vrn-java-06\\src\\test\\resources\\test.zip").getPath());
         Calculator calculator = new Calculator();
         System.out.println("Calculator get number of 1: " +
                 calculator.getNumberOfChar("C:\\Users\\user\\Desktop\\vrn-java-06\\src\\test\\resources\\test.zip", '1'));
 
         System.out.println("Calculator get max length: " +
-                calculator.getMaxWordLength("some_path\\test1.zip"));
+                calculator.getMaxWordLength("C:\\Users\\user\\Desktop\\vrn-java-06\\src\\test\\resources\\test1.zip"));
     }
 }
